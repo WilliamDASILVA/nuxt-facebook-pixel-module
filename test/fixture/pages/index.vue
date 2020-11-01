50ce676a203a801540cdbbe577b850448a4101de
William DA SILVA <william.da.silva@outlook.com>
2020-11-01 18:02:07 +0100

    feat: add disable method (#19)

--- test/fixture/pages/index.vue
+++ test/fixture/pages/index.vue
@@ -1,10 +1,15 @@
 <template>
     <div>
         Works!
+        <nuxt-link to="/test">Navigate</nuxt-link>
     </div>
 </template>
 
 <script>
     export default {
+        mounted () {
+            this.$fb.enable()
+            this.$fb.track()
+        }
     }
 </script>
